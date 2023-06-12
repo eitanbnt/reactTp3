d8d697cd5015fe2f4d6f4c8b535123d6c22ecc62
eitan Benita <e.benita@lyon.ort.asso.fr>
2023-06-12 15:52:51 +0200

    affichage des cocktails

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    Cocktail();
+  }, []);
+
+  const Cocktail = async () => {
+    try {
+      fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail&api_key=1`
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          if (json) {
+            setData(json.drinks);
+            console.log(json.drinks);
+          }
+        });
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  if (error) {
+    return <Text>Une erreur s'est produite: {error.message}</Text>;
+  }
+
+  if (!data) {
+    return <Text>Chargement en cours...</Text>;
+  }
+
+  //console.log(data);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.background}>
+      <ScrollView>
+        {data.map((item) => (
+          <View key={item.idDrink} style={styles.Cocktail}>
+            <Text>
+              {item.strDrink}
+            </Text>
+            <Image source={{
+              uri: `${item.strDrinkThumb}`
+            }} style={styles.Icon} />
+          </View>
+          ))}
+      </ScrollView>
+      <StatusBar style='auto'/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Icon: {
+    marginTop: 10,
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+  },
+  Cocktail: {
+    alignItems: "center",
+    marginHorizontal: 10,
+    backgroundColor: "#f5f5dc36",
+    borderColor: "grey",
+    borderRadius: 5,
+    borderWidth: 2,
+    width: 250,
+    height: 270,
+    marginBottom: 20,
   },
 });
